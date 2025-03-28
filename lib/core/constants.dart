406f3a9a1a4c44255a22b90157dc53fd39e2c04a
adarshnagrikar14 <adarshnagrikar1404@gmail.com>
2025-03-28 14:40:49 +0530

    chore: update API key and add new URL for image generation

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,5 @@
 class Constants {
-  static const String apiKey = 'AIzaSyDwClwNn2DQZCnqzoOnPM9WwN_01ZjTdsM';
+  static const String apiKey = 'AIzaSyAygtJry_8sCcVotWp8OTy60W7W7bKCQZI';
+  static const String url =
+      'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-exp-image-generation:generateContent';
 }
-
